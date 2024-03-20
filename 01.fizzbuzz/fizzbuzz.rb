5e47b444c1acc3d84798666195dbeeb64037d9aa
marina0088 <marina.isobe@gmail.com>
2024-03-20 14:33:29 +0900

    メソッドを無くしました

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,15 +1,12 @@
-
-  def fizzbuzz(number)
-    (1..20).each do |number| 
-    if number % 3 == 0 && number % 5 == 0
-      puts "FizzBuzz"    
-    elsif number % 3 == 0
-      puts "Fizz"
-    elsif number % 5 == 0
-      puts "Buzz"
-    else
-      puts number
-    end
-  end  
-  puts fizzbuzz(number) 
+#!/usr/bin/env ruby
+(1..20).each do |number|
+  if number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
 end
