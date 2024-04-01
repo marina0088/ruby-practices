770aee8bc99a132fc71e18dd8ebc263f62d5e19f
marina0088 <marina.isobe@gmail.com>
2024-04-01 19:29:12 +0900

    アドバイスをもとに修正しました

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+STRIKE_SCORE=10
 score = ARGV[0]
 scores = score.split(',')
 
@@ -8,39 +9,37 @@ shots = []
 scores.each do |s|
   case s
   when 'X'
-    shots << 10
+    shots << STRIKE_SCORE
     shots << 0
   when '/'
-    shots << 10 - shots.last
+    shots << STRIKE_SCORE - shots.last
   else
     shots << s.to_i
   end
 end
 
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
+frames = shots.each_slice(2).to_a
 
 point = 0
 frames.each_with_index do |frame, index|
   point += frame.sum
+  next_frame = frames[index + 1]
 
   if index < 9
 
-    if frame[0] == 10
-      point += if frames[index + 1][0] == 10
-                 frames[index + 1][0] + frames[index + 2][0]
-               else
-                 frames[index + 1].sum
-               end
+    if frame[0] == STRIKE_SCORE
+      point += if next_frame[0] == STRIKE_SCORE
+        next_frame[0] + frames[index + 2][0]
+      else
+        next_frame.sum
+      end
 
-    elsif frame.sum == 10 && frame[0] != 10
-      point += frames[index + 1][0]
+    elsif frame.sum == STRIKE_SCORE && frame[0] != STRIKE_SCORE
+      point += next_frame[0]
     end
   elsif index == 9
 
-    point += frames[10].last if (frame[0] == 10 || frame.sum == 10) && (frames[10].size == 3 && frames[10].sum > 10)
+    point += frames[10].last if (frame[0] == STRIKE_SCORE || frame.sum == STRIKE_SCORE) && (frames[10].size == 3 && frames[10].sum > STRIKE_SCORE)
   end
 end
 
